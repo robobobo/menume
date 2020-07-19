24930fc0acbe080e9c0e611ceb855f2db8002f28
Robert Boland <robertboland1@gmail.com>
2020-07-19 18:36:01 +0100

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(config('app.env') === 'production' || config('app.env') === 'testing') {
+            \URL::forceScheme('https');
+        }
     }
 }
