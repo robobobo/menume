ebc17727f01a8d29948043c61c6b477cbc913337
Robert Boland <robertboland1@gmail.com>
2020-08-06 22:25:24 +0100

    add qr code to establishment list

--- resources/views/establishment/index.blade.php
+++ resources/views/establishment/index.blade.php
@@ -5,14 +5,18 @@
 @section('content')
 <section class="section">
 <h1 class="is-size-1 has-text-centered">All Establishments</h1>
-<h3 class="is-size-3 has-text-centered">Choose an establishment to see their menus</h3>
+<h3 class="is-size-3 has-text-centered">Choose an establishment to see their menus or scan the QR code</h3>
 <div class="columns">
     <div class="column is-half is-offset-one-quarter">
         @foreach ($establishments as $establishment)
         <a href="{{route('establishment.show',$establishment)}}">
             <div class="card has-background-primary mb-3">
                 <div class="card-content">
-                    <div class="is-size-3 has-text-white">{{$establishment->name}}</div>
+                    <div class="is-size-3 has-text-white has-text-centered">
+                        <img src="{!!$establishment->qrCodeURL()!!}" alt="">
+                        <hr>
+                        {{$establishment->name}}
+                    </div>
                 </div>
             </div>
         </a>
